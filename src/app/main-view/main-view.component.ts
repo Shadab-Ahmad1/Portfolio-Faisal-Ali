b101a1998c9abf51784eaf49464beefa9836c890
ashadab124@gmail.com <ashadab124@gmail.com>
2023-06-02 23:26:25 +0500

    change background

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-main-view',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainViewComponent implements OnInit {
 
+  backgroundImages: string[] = [
+    'url(/assets/img/Texture-1.jpg)',
+    'url(/assets/img/Texture-2.jpg)',
+    'url(/assets/img/Texture-3.jpg)'
+  ];
+    currentBackgroundIndex: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.changeBackground();
+    }, 3000);
   }
+  changeBackground(): void {
+    this.currentBackgroundIndex = (this.currentBackgroundIndex + 1) % this.backgroundImages.length;
+  }
+
 
 }
